3b85c360a81731a055e497053a38f7bcae6f7e5c
JulioVianaDev <juliovianadev@gmail.com>
2022-04-14 17:33:25 -0300

    subindo o realtime chat 13:33

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          has_many :tweets
+  scope :all_except,-> (user) {where.not(id: user)}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :rooms
   resources :tweets
   devise_for :users
   root 'pages#home'
